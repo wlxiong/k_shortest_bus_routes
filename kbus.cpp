44d7ff9edbecbdb4c7401f7af6eeb42bbc15df1a
Yiliang Xiong <wlxiong@gmail.com>
2013-06-08 22:38:18 +0800

    set to K&R code style

--- kbus.cpp
+++ kbus.cpp
@@ -15,8 +15,7 @@ public:
     short ticket, type, nUp, nDown;
     short up[MAX], down[MAX];
 
-    LINE()
-    {
+    LINE() {
         nUp = 0;
         nDown = 0;
     }
@@ -28,12 +27,10 @@ public:
     short n;
     short lines[MAX], iUp[MAX], iDown[MAX];
 
-    STOP()          //initialize the stop object
-    {
+    STOP() {        //initialize the stop object
         int i;
         n = 0;
-        for (i = 0; i < MAX; i++)
-        {
+        for (i = 0; i < MAX; i++) {
             iUp[i] = MAX;
             iDown[i] = MAX;
         }
@@ -47,8 +44,7 @@ public:
     bool ex;
     short cost[MAX], pre[MAX], pos[MAX], pline[MAX], it[MAX];
 
-    void init()
-    {
+    void init() {
         n = 0;
         ex = false;
     }
@@ -68,29 +64,23 @@ void init_adj()
     // initialize the adjacent matrix
     int i, j, k, x, y;
 
-    for (i = 0; i <= maxp; i++)
-    {
+    for (i = 0; i <= maxp; i++) {
         for (j = 0; j <= maxp; j++)
             adj[i][j] = M;
         adj[i][i] = 0;
     }
 
-    for (k = 1; k <= maxl; k++)
-    {
-        for (i = 0; i < Lines[k].nUp; i++)
-        {
+    for (k = 1; k <= maxl; k++) {
+        for (i = 0; i < Lines[k].nUp; i++) {
             x = Lines[k].up[i];
-            for (j = i + 1; j < Lines[k].nUp; j++)
-            {
+            for (j = i + 1; j < Lines[k].nUp; j++) {
                 y = Lines[k].up[j];
                 adj[y][x] = 1;
             }
         }
-        for (i = 0; i < Lines[k].nDown; i++)
-        {
+        for (i = 0; i < Lines[k].nDown; i++) {
             x = Lines[k].down[i];
-            for (j = i + 1; j < Lines[k].nDown; j++)
-            {
+            for (j = i + 1; j < Lines[k].nDown; j++) {
                 y = Lines[k].down[j];
                 adj[y][x] = 1;
             }
@@ -112,17 +102,14 @@ void dijkstra(short n, short s, short map[MAXP][MAXP], short cost[MAXP])
     cost[s] = 0;
 
     Q.insert(node(0, s));
-    while (Q.empty() == false)
-    {
+    while (Q.empty() == false) {
         node top = *Q.begin();
         Q.erase(Q.begin());
         int index = top.second;
 
-        for (i = 1; i <= n; i++)
-        {
+        for (i = 1; i <= n; i++) {
             int weight = map[index][i];
-            if (cost[i] > cost[index] + weight)
-            {
+            if (cost[i] > cost[index] + weight) {
                 if (cost[i] != M)
                     Q.erase(Q.find(node(cost[i], i)));
                 cost[i] = cost[index] + weight;
@@ -139,20 +126,16 @@ bool load()
     short line, stop, last, i, j;
     char ch;
 
-    if (fin.fail())
-    {
+    if (fin.fail()) {
         cout << "\nCan't open file busline.txt!\n\n";
         return false;
-    }
-    else
-    {
+    } else {
         cout << "\nIt will take a few seconds to load the network data...\n";
     }
     maxp = 0;
     maxl = 0;
     ch = fin.peek();
-    while (ch != 'E')
-    {
+    while (ch != 'E') {
         ch = fin.get();
         fin >> line;
         if (line > maxl)
@@ -161,8 +144,7 @@ bool load()
         Lines[line].type = 1;
         ch = fin.get();
         ch = fin.peek();
-        while (ch != '\n')
-        {
+        while (ch != '\n') {
             ch = fin.get();
             fin >> stop;
             if (stop > maxp)
@@ -175,47 +157,35 @@ bool load()
             Stops[stop].n++;
         }
         ch = fin.peek();
-        if (ch != 'L' && ch != 'E')
-        {
-            while (ch != '\n')
-            {
+        if (ch != 'L' && ch != 'E') {
+            while (ch != '\n') {
                 ch = fin.get();
                 fin >> stop;
                 if (stop > maxp)
                     maxp = stop;
                 ch = fin.get();
                 Lines[line].down[ Lines[line].nDown ] = stop;
-                if (Stops[stop].n < 1)
-                {
+                if (Stops[stop].n < 1) {
                     Stops[stop].lines[ Stops[stop].n ] = line;
                     Stops[stop].n++;
-                }
-                else
-                {
+                } else {
                     if (Stops[stop].lines[ Stops[stop].n - 1 ] == line)
                         Stops[stop].iDown[ Stops[stop].n - 1 ] = Lines[line].nDown;
-                    else
-                    {
+                    else {
                         Stops[stop].iDown[ Stops[stop].n ] = Lines[line].nDown;
                         Stops[stop].n++;
                     }
                 }
                 Lines[line].nDown++;
             }
-        }
-        else
-        {
-            if ( Lines[line].up[0] == Lines[line].up[ Lines[line].nUp - 1 ])
-            {
+        } else {
+            if ( Lines[line].up[0] == Lines[line].up[ Lines[line].nUp - 1 ]) {
                 Lines[line].type = 0;
                 for (i = 0, j = Lines[line].nUp; i < Lines[line].nUp - 1; i++, j++)
                     Lines[line].up[j] = Lines[line].up[i];
-            }
-            else
-            {
+            } else {
                 last = 0;
-                for (i = Lines[line].nUp - 1, j = 0; i >= 0; i--, j++)
-                {
+                for (i = Lines[line].nUp - 1, j = 0; i >= 0; i--, j++) {
                     Lines[line].down[j] = Lines[line].up[i];
                     Stops[ Lines[line].down[j] ].iDown[ Stops[ Lines[line].down[j] ].n - 1 ] = j;
                 }
@@ -232,13 +202,10 @@ bool input(short &o, short &d, short &k, short &g)
 {
     // input the O&D, maximum number of transfers and k
     cout << "\n>The origin and the destination, separated by a blank: ";
-    if (!(cin >> o >> d))
-    {
+    if (!(cin >> o >> d)) {
         cout << endl;
         return false;
-    }
-    else
-    {
+    } else {
         cout << ">The maximum number of transfers: ";
         cin >> g;
         cout << ">Search the first k shortest routes,then k= ";
@@ -259,21 +226,18 @@ void output(short O, short D)
     cout << endl;
     if (Nodes[D].n == 0)
         cout << "No available path!\n";
-    else
-    {
+    else {
         for (i = 0; i < Nodes[D].n; i++)
             s.push_back(pair<short, short>(Nodes[D].cost[i], i));
         sort(s.begin(), s.end());
 
-        for (p = s.begin(), i = 1; p != s.end(); p++, i++)
-        {
+        for (p = s.begin(), i = 1; p != s.end(); p++, i++) {
             cout << setfill(' ');
             cout << "Path" << setw(2) << i << " (Time" << setw(3) << Nodes[D].cost[p->second] << ", Transfer" << setw(2) << Nodes[D].it[p->second] << ')';
             pre = D;
             pos = p->second;
             out.clear();
-            while (pre != O)
-            {
+            while (pre != O) {
                 out.push_back(pre);
                 out.push_back(Nodes[pre].pline[pos]);
                 tmp = pre;
@@ -297,8 +261,7 @@ void search()
     short iline, ipos, inode, icost, max, n;
     queue<short> Q;
 
-    while (input(O, D, K, G))
-    {
+    while (input(O, D, K, G)) {
         for (i = 0; i < maxp; i++)
             Nodes[i].init();
         dijkstra(maxp, D, adj, iter);
@@ -309,8 +272,7 @@ void search()
         Nodes[O].it[ Nodes[O].n ] = -1;
         Nodes[O].n++;
         Q.push(O);
-        while (Q.empty() == false)
-        {
+        while (Q.empty() == false) {
             s = Q.front();
             Q.pop();
             if (Nodes[s].ex || s == D)
@@ -318,27 +280,20 @@ void search()
             else
                 Nodes[s].ex = true;
 
-            for (i = 0; i < Stops[s].n; i++)
-            {
+            for (i = 0; i < Stops[s].n; i++) {
                 iline = Stops[s].lines[i];
-                for (j = 0; j < Nodes[s].n; j++)
-                {
-                    if (Nodes[s].it[j] + iter[s] <= G)
-                    {
+                for (j = 0; j < Nodes[s].n; j++) {
+                    if (Nodes[s].it[j] + iter[s] <= G) {
 
-                        if (iline != Nodes[s].pline[j])
-                        {
-                            if (Lines[iline].type == 1)
-                            {
+                        if (iline != Nodes[s].pline[j]) {
+                            if (Lines[iline].type == 1) {
                                 ipos = Stops[s].iUp[i];
-                                for (ii = ipos + 1; ii < Lines[iline].nUp; ii++) //Upline
-                                {
+                                for (ii = ipos + 1; ii < Lines[iline].nUp; ii++) { //Upline
                                     inode = Lines[iline].up[ii];
                                     icost = Nodes[s].cost[j] + (ii - ipos) * adj_time + change_time;
                                     n = Nodes[inode].n;
 
-                                    if (n < K)
-                                    {
+                                    if (n < K) {
                                         Q.push(inode);
                                         Nodes[inode].it[n] = Nodes[s].it[j] + 1;
                                         Nodes[inode].pre[n] = s;
@@ -346,12 +301,9 @@ void search()
                                         Nodes[inode].pline[n] = iline;
                                         Nodes[inode].cost[n] = icost;
                                         Nodes[inode].n++;
-                                    }
-                                    else
-                                    {
+                                    } else {
                                         max = (int)(max_element(Nodes[inode].cost, &Nodes[inode].cost[n]) - Nodes[inode].cost);
-                                        if (Nodes[inode].cost[max] > icost)
-                                        {
+                                        if (Nodes[inode].cost[max] > icost) {
                                             Q.push(inode);
                                             Nodes[inode].it[max] = Nodes[s].it[j] + 1;
                                             Nodes[inode].pre[max] = s;
@@ -362,14 +314,12 @@ void search()
                                     }
                                 }
                                 ipos = Stops[s].iDown[i];
-                                for (ii = ipos + 1; ii < Lines[iline].nDown; ii++) //Downline
-                                {
+                                for (ii = ipos + 1; ii < Lines[iline].nDown; ii++) { //Downline
                                     inode = Lines[iline].down[ii];
                                     icost = Nodes[s].cost[j] + (ii - ipos) * adj_time + change_time;
                                     n = Nodes[inode].n;
 
-                                    if (n < K)
-                                    {
+                                    if (n < K) {
                                         Q.push(inode);
                                         Nodes[inode].it[n] = Nodes[s].it[j] + 1;
                                         Nodes[inode].pre[n] = s;
@@ -377,12 +327,9 @@ void search()
                                         Nodes[inode].pline[n] = iline;
                                         Nodes[inode].cost[n] = icost;
                                         Nodes[inode].n++;
-                                    }
-                                    else
-                                    {
+                                    } else {
                                         max = (int)(max_element(Nodes[inode].cost, &Nodes[inode].cost[n]) - Nodes[inode].cost);
-                                        if (Nodes[inode].cost[max] > icost)
-                                        {
+                                        if (Nodes[inode].cost[max] > icost) {
                                             Q.push(inode);
                                             Nodes[inode].it[max] = Nodes[s].it[j] + 1;
                                             Nodes[inode].pre[max] = s;
@@ -392,18 +339,14 @@ void search()
                                         }
                                     }
                                 }
-                            }
-                            else
-                            {
+                            } else {
                                 ipos = Stops[s].iUp[i];
-                                for (ii = ipos + 1, k = 0; k < Lines[iline].nUp; k++, ii++) //Circle
-                                {
+                                for (ii = ipos + 1, k = 0; k < Lines[iline].nUp; k++, ii++) { //Circle
                                     inode = Lines[iline].up[ii];
                                     icost = Nodes[s].cost[j] + (ii - ipos) * adj_time + change_time;
                                     n = Nodes[inode].n;
 
-                                    if (n < K)
-                                    {
+                                    if (n < K) {
                                         Q.push(inode);
                                         Nodes[inode].it[n] = Nodes[s].it[j] + 1;
                                         Nodes[inode].pre[n] = s;
@@ -411,12 +354,9 @@ void search()
                                         Nodes[inode].pline[n] = iline;
                                         Nodes[inode].cost[n] = icost;
                                         Nodes[inode].n++;
-                                    }
-                                    else
-                                    {
+                                    } else {
                                         max = (int)(max_element(Nodes[inode].cost, &Nodes[inode].cost[n]) - Nodes[inode].cost);
-                                        if (Nodes[inode].cost[max] > icost)
-                                        {
+                                        if (Nodes[inode].cost[max] > icost) {
                                             Q.push(inode);
                                             Nodes[inode].it[max] = Nodes[s].it[j] + 1;
                                             Nodes[inode].pre[max] = s;
